c0ef8d4c79354e6cc5366d0b7ab564e8a1eac9e4
Liza Katz <lizka.k@gmail.com>
2025-05-27 16:50:27 +0300

    chore: cookies

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,10 +1,15 @@
 import { getCookie, setCookie, deleteCookie } from "./cookieUtils";
 
+const DEFAULT_AUTH_COOKIE_HOURS = 24;
+
 export const getAuthCookie = (): string | null => {
   return getCookie("auth");
 };
 
-export const setAuthCookie = (authString?: string): void => {
-  if (authString) setCookie("auth", authString, 24);
-  else deleteCookie("auth");
+export const setAuthCookie = (authString?: string, hours: number = DEFAULT_AUTH_COOKIE_HOURS): void => {
+  if (authString) {
+    setCookie("auth", authString, hours);
+  } else {
+    deleteCookie("auth");
+  }
 };
--- src/utils/cookieUtils.ts
+++ src/utils/cookieUtils.ts
@@ -2,7 +2,7 @@ export function setCookie(name: string, value: string, hours: number): void {
   const date = new Date();
   date.setTime(date.getTime() + (hours * 60 * 60 * 1000));
   const expires = "expires=" + date.toUTCString();
-  document.cookie = `${name}=${value};${expires};path=/`;
+  document.cookie = `${name}=${value}; ${expires}; path=/; SameSite=Strict`;
 }
 
 export function getCookie(name: string): string | null {
